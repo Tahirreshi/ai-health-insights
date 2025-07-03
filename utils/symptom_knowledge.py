77469001501d8a1b1cb029d72aebd67a2e2ed31e
Tahir Irshad <reshit141@gmail.com>
2025-07-03 07:16:02 +0530

    symptom_knowledge.py

---
+++ utils/symptom_knowledge.py
@@ -0,0 +1,30 @@
+symptom_database = [
+    {
+        "symptoms": ["fever", "cough", "fatigue"],
+        "conditions": ["Common Cold", "Flu", "COVID-19"]
+    },
+    {
+        "symptoms": ["chest pain", "shortness of breath"],
+        "conditions": ["Heart Attack", "Asthma", "Anxiety"]
+    },
+    {
+        "symptoms": ["headache", "nausea"],
+        "conditions": ["Migraine", "Dehydration"]
+    },
+    {
+        "symptoms": ["stomach pain", "diarrhea"],
+        "conditions": ["Food Poisoning", "Gastroenteritis"]
+    },
+    {
+        "symptoms": ["rash", "itchiness"],
+        "conditions": ["Allergy", "Eczema"]
+    },
+    {
+        "symptoms": ["high blood pressure", "dizziness"],
+        "conditions": ["Hypertension", "Vertigo"]
+    },
+    {
+        "symptoms": ["joint pain", "swelling"],
+        "conditions": ["Arthritis", "Gout"]
+    }
+]
